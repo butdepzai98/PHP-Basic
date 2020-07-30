f13e0ac749c40f28b2aecbb3be0ec28ded12bbe9
XuanVinh <ubuntu@vxo@.com>
2020-07-30 16:48:06 +0700

    bt7

--- __BT7.php
+++ __BT7.php
@@ -9,5 +9,16 @@ $str = 'co chac yeu la day';
 
 function countNguyenAm($str)
 {
-    
+    $dem = 0;
+    $str = str_replace(' ','',$str);
+
+    for ($i=0; $i < strlen($str); $i++) { 
+        if (($str[$i] == 'a')||($str[$i] == 'e')||($str[$i] == 'i')||($str[$i] == 'o')||($str[$i] == 'u')) {
+            $dem++;
+        }
+    }
+
+    return $dem;
 }
+
+echo countNguyenAm($str);

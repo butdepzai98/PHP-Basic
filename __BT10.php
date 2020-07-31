46abb3a99c6b2fcb06ae31252c7fa942720a6305
Xuan Vinh <winxuanvinh98@gmail.com>
2020-07-31 09:38:08 +0700

    Finished 10 execute

--- __BT10.php
+++ __BT10.php
@@ -5,3 +5,96 @@ Bất kỳ ký tự nào không phải là nguyên âm nên giữ nguyên vị t
     VD:
         Hello    -> Holle
         Tomatoes -> Temotaos
+
+<?php
+echo '<br>';
+
+$str = 'Hel loluap';
+$str2 = 'Hel loa';
+
+function laNguyenAm($str)
+{
+    if (($str == 'a')||($str == 'e')||($str == 'i')||($str == 'o')||($str == 'u')) {
+        return true;
+    }
+    else
+        return false;
+}
+
+function countNguyenAm($str)
+{
+    $dem = 0;
+    $str = str_replace(' ','',$str);
+
+    for ($i=0; $i < strlen($str); $i++) { 
+        if (laNguyenAm($str[$i])) {
+            $dem++;
+        }
+    }
+
+    return $dem;
+}
+
+function revert2Vitri($str, $vt1, $vt2)
+{
+    $temp = $str[$vt1];
+    $str[$vt1] = $str[$vt2];
+    $str[$vt2] = $temp;
+
+    return $str;
+}
+
+function revertNgAm($str)
+{
+    $str = str_replace(' ','',$str);
+    $num_NgAm = countNguyenAm($str);
+
+    $array_key = [];
+    $array_key_revert = [];
+
+    if($num_NgAm == 1)
+    {
+        echo $num_NgAm;
+    }
+    else if($num_NgAm%2 == 0)
+    {
+        for ($i=0; $i < strlen($str); $i++) { 
+            if(laNguyenAm($str[$i]))
+            {
+                array_push($array_key, $i);
+                $array_key_revert = array_reverse($array_key);
+            }
+        }
+
+        //Tiến hành đảo ngược
+        for ($j=0; $j < sizeof($array_key)/2; $j++) { 
+            $str = revert2Vitri($str, $array_key[$j], $array_key_revert[$j]);
+        }
+
+        return $str;
+    }
+    else
+    {
+        for ($i=0; $i < strlen($str); $i++) { 
+            if(laNguyenAm($str[$i]))
+            {
+                array_push($array_key, $i);
+                $array_key_revert = array_reverse($array_key);
+            }
+        }
+
+        //Tiến hành đảo ngược
+        for ($j=0; $j <= sizeof($array_key)/2; $j++) { 
+            $str = revert2Vitri($str, $array_key[$j], $array_key_revert[$j]);
+        }
+
+        return $str;
+        // echo (int)(sizeof($array_key)/2);
+        // echo '<br>';
+        // var_dump($array_key);
+        // echo '<br>';
+        // var_dump($array_key_revert);
+    }
+}
+
+echo revertNgAm($str2);
--- __BT8.php
+++ __BT8.php
@@ -6,3 +6,22 @@ Không cần phải có dấu phẩy ở cuối số.
 
 VD: 
     commas(-1000000.123) == "-1,000,000.123"
+
+<?php
+echo '<br>';
+
+$number = -1000000.123;
+echo '<br>';
+echo number_format($number, 3,'.',',');
+
+echo '<br>';
+echo '<br>';
+?>
+
+#   --------------------------- number_format ----------------------
+number_format ( float $number , $decimals = 0 , $dec_point = "." , $thousands_sep = "," );
+
+-   $number là số cần định dạng.
+-   $decimals là số chữ số thập phân muốn lấy, mặc định sẽ là 0.
+-   $dec_point là kí tự phân cách với phần thập phân, mặc định là dấu " . ".
+-   $thousands_sep là kí tự phân cách giữa các nhóm hàng nghìn, mặc định là dấu " , " 
